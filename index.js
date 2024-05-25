22323a5ce44410cad174de3fa6197ed776154f68
18Ronnel <nitoyaronnel401@gmail.com>
2024-05-25 17:55:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,23 +18,23 @@ function countLike2() {
 
 btnLike2.addEventListener("click", countLike2);
 
-const btnDislike1 = document.getElementById("btnLike1");
+const btnDislike1 = document.getElementById("btnDislike1");
 const count3 = document.getElementById("count3");
 
 function countDislike1() {
-  let likeCounts = parseInt(count3.value) + 1;
-  count3.textContent = likeCounts.toString();
+  let DislikeCounts = parseInt(count3.value) + 1;
+  count3.textContent = DislikeCounts.toString();
 }
-btnDislike1.addEventListener("click", countLike1);
+btnDislike1.addEventListener("click", countDisLike1);
 
 
 
 
-const btnDislike2 = document.getElementById("btnLike2");
+const btnDislike2 = document.getElementById("btnDislike2");
 const count4 = document.getElementById("count4");
 
 function countDislike2() {
-  let likeCounts = parseInt(count4.value) + 1;
-  count4.textContent = likeCounts.toString();
+  let DislikeCounts = parseInt(count4.value) + 1;
+  count4.textContent = DislikeCounts.toString();
 }
-btnDislike2.addEventListener("click", countLike2)
+btnDislike2.addEventListener("click", countDislike2)
