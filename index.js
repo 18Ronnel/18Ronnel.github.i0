5eb8b9722bd00ee17bb47b329919a1c54f0086ac
18Ronnel <nitoyaronnel401@gmail.com>
2024-06-08 09:39:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const btnLike1 = document.getElementById("btnLike1")
+qconst btnLike1 = document.getElementById("btnLike1")
 const count1 = document.getElementById("count1")
 
 function countLike1() {
@@ -38,3 +38,11 @@ function countDislike2() {
 }
 
 btnDislike2.addEventListener("click", countDislike2)
+
+
+const submit= document.getElementById("submit")
+const comment = document.getElementById("comment")
+const commentbox = document.getElementById("commentbox")
+function submitComment(){
+  commentbox.textContent += comment.value.toString() + "\n"
+}  
